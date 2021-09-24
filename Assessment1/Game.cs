bb8ae195f7b797265949bc174958c0e0576451ea
mikeLofton <michaellofton32@gmail.com>
2021-09-24 09:40:14 -0500

    Added Start,Update,End

--- Assessment1/Game.cs
+++ Assessment1/Game.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
 
 namespace Assessment1
 {
     class Game
     {
         public void Run()
+        {
+
+        }
+
+        private void Start()
+        {
+
+        }
+
+        private void Update()
+        {
+
+        }
+
+        private void End()
         {
 
         }
