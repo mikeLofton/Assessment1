12de5bdcef3e4f52d46df72ae9eb0a6498e9a3d7
mikeLofton <michaellofton32@gmail.com>
2021-09-24 08:57:21 -0500

    Assessment start

---
+++ Assessment1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Assessment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a new instance of Game
+            Game game = new Game();
+
+            //Run the Game
+            game.Run();
+        }
+    }
+}
