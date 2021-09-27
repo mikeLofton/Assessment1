d19c043f46bacf5fb6c73c86a8975a8536e470e0
mikeLofton <michaellofton32@gmail.com>
2021-09-27 09:30:13 -0500

    Added GetInput

--- Assessment1/Game.cs
+++ Assessment1/Game.cs
@@ -7,6 +7,8 @@ namespace Assessment1
 {
     class Game
     {
+        private bool gameOver;
+
         public void Run()
         {
 
@@ -25,6 +27,63 @@ namespace Assessment1
         private void End()
         {
 
+        }
+
+        private void Save()
+        {
+
+        }
+
+        private bool Load()
+        {
+
+        }
+
+        /// <summary>
+        /// Function recieves an input from the player and will return either an input recieved integer
+        /// or label the input invalid.
+        /// </summary>
+        /// <param name="description">The set up or scenerio</param>
+        /// <param name="options">The options the player is given</param>
+        /// <returns>Returns the input recieved integer</returns>
+        private int GetInput(string description, params string[] options)
+        {
+            string choice = "";
+            int inputRecieved = -1;
+
+            while (inputRecieved == -1)
+            {
+                Console.WriteLine(description);
+                for (int i = 0; i < options.Length; i++)
+                {
+                    Console.WriteLine((i + 1) + ". " + options[i]);
+                }
+                Console.Write("> ");
+
+                choice = Console.ReadLine();
+
+                if (int.TryParse(choice, out inputRecieved))
+                {
+                    inputRecieved--;
+                    if (inputRecieved < 0 || inputRecieved >= options.Length)
+                    {
+                        inputRecieved = -1;
+                        //Display error message
+                        Console.WriteLine("Invalid Input");
+                        Console.ReadKey(true);
+                    }
+                }
+                else
+                {
+                    inputRecieved = -1;
+                    Console.WriteLine("Invalid Input");
+                    Console.ReadKey(true);
+                }
+
+                Console.Clear();
+            }
+
+            return inputRecieved;
         }
     }
 }
